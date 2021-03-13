1556e56beb5187f1697662601f5b29d1554504cc
Angelica <Angelica137@users.noreply.github.com>
2021-03-13 20:55:11 +0000

    initialise one Exciting object

--- TestExciting.java
+++ TestExciting.java
@@ -1,15 +1,14 @@
 
 public class TestExciting {
-	
- public static void main (String args[]) {
-	 
-	 //after 1a) create an Exciting object, and display the details for the object
-	 
-	 
-	//after 1b) create an Exciting object, and display the details for the object
-	 
-	 
-	 
- }
+
+	public static void main(String args[]) {
+
+		// after 1a) create an Exciting object, and display the details for the object
+		Exciting e = new Exciting();
+		e.displayDetails();
+
+		// after 1b) create an Exciting object, and display the details for the object
+
+	}
 
 }
